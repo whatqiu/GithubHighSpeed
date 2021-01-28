a064ebbc415e35f13023e21d3b84e19a530ac886
yjacks <67487960+yjacks@users.noreply.github.com>
2021-01-28 15:49:28 +0800

    Update Github加速.py

--- Github加速.py
+++ Github加速.py
@@ -8,11 +8,13 @@ from bs4 import BeautifulSoup
 github = requests.get('https://github.com.ipaddress.com/')
 github_ssl = requests.get('https://fastly.net.ipaddress.com/github.global.ssl.fastly.net')
 github_cdn = requests.get('https://github.com.ipaddress.com/assets-cdn.github.com')
-github_cdn.encoding = github_ssl.encoding = github.encoding = 'utf8'
+github_image = requests.get('https://githubusercontent.com.ipaddress.com/user-images.githubusercontent.com')
+github_cdn.encoding = github_ssl.encoding = github.encoding = github_image.encoding = 'utf8'
 
 gtext = BeautifulSoup(github.text, 'html.parser')
 gstext = BeautifulSoup(github_ssl.text, 'html.parser')
 gctext = BeautifulSoup(github_cdn.text, 'html.parser')
+gitext = BeautifulSoup(github_image.text, 'html.parser')
 
 
 def re_ip():
@@ -22,7 +24,9 @@ def re_ip():
         re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b",
                    str(gstext.find('table', attrs={'class': 'dnsinfo'})))[0],
         re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b",
-                   str(gctext.find('table', attrs={'class': 'dnsinfo'})))[0]
+                   str(gctext.find('table', attrs={'class': 'dnsinfo'})))[0],
+         re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b",
+                   str(gitext.find('table', attrs={'class': 'dnsinfo'})))[0]
     ]
 
 
@@ -30,7 +34,8 @@ def re_hosts():
     score_ip = re_ip()
     return [score_ip[0] + ' github.com\n',
             score_ip[1] + ' assets-cdn.github.com\n',
-            score_ip[2] + ' github.global.ssl.fastly.net\n']
+            score_ip[2] + ' github.global.ssl.fastly.net\n',
+            score_ip[3] + 'user-images.githubusercontent.com\n']
 
 
 if __name__ == '__main__':
